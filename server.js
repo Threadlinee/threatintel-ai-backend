c616ac0c6507d1d9a5c19ab3bd34e1bc76fa7055
𝟶𝚡𝙰𝚋𝚢𝚜𝚜 <dionabazi5@gmail.com>
2025-06-14 21:31:52 +0200

    wip/14

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
 const allowedOrigins = [
   'https://threatintel-ai.vercel.app',
